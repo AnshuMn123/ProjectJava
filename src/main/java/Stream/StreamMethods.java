6f4b04577a58553ca5272a3b4d0c5dbb952a8914
AnshuMn123 <asingh.taxilla.com>
2023-06-16 12:06:50 +0530

    stream

---
+++ src/main/java/Stream/StreamMethods.java
@@ -0,0 +1,24 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMethods {
+    public static void main(String arg[]){
+        List<Integer> l = Arrays.asList(3,6,2,4,7,8,9);
+
+//        Stream<Integer> s1 = l.stream();
+//        Stream<Integer> s2 = s1.filter(n -> n%2 == 0);
+//        Stream<Integer> s3 = s2.map(n -> n*2);
+//        int s4 = s3.reduce(0, (c,e) -> c + e);
+
+        int result = l.stream()
+                    .filter(n-> n%2 == 0)
+                    .map(n -> n*2)
+                    .reduce(0, (c, e) -> c+e);
+
+        System.out.println(result);
+
+    }
+}
